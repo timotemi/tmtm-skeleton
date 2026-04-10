f6209dd0ec9a9b395cfc8dfb368900304bdfa800
bagjiwon <bagjiwon0@gmail.com>
2026-04-10 10:50:42 +0900

    feat: 입력창 기능 추가

--- src/views/MonthlySummaryView.vue
+++ src/views/MonthlySummaryView.vue
@@ -1,9 +1,35 @@
 <template>
   <div class="page">
     <h1>월별요약</h1>
-    <p>여기에 월별 요약 화면이 들어갈 예정입니다.</p>
+    <table>
+      <thead>
+        <tr>
+          <th>몇 월</th>
+          <th>지출 총계</th>
+          <th>수입 총계</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="m in 12" :key="m">
+          <th>{{ m }}월</th>
+          <td>지출</td>
+          <td>수입</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+const authStore = useAuthStore();
+
+const userid = authStore.user.email;
+
+const totalExpense = async () => {
+  let response = await axios.get(BASEURL);
+};
+</script>
 
 <style scoped>
 .page {
